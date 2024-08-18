7144e885a568ddc79ae455fef77999922d514925
felipe-sbm <felipesbm2005@gmail.com>
2024-08-18 16:49:54 -0300

    feat: Update church dates and descriptions
    This commit updates the dates and descriptions of the church events on the" Cronograma" page. The previous dates were incorrect, and the descriptions were not clear. Now, the dates are accurate, and the descriptions provide more information about each culto event. This improves the accuracy and clarity of the information displayed on the page.

--- app/pages/cronograma/page.tsx
+++ app/pages/cronograma/page.tsx
@@ -1,18 +1,18 @@
 export default function Cronograma() {
   return (
     <main>
-      <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+      <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 mb-4">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
           <div className="rounded-2xl border border-sky-600 p-6 shadow-md ring-1 ring-sky-600 sm:order-last sm:px-8 lg:p-12">
             <div className="text-center">
-              <h2 className="text-lg font-medium text-zinc-500 dark:text-zinc-100">
+              <h2 className="text-sm font-medium text-zinc-500 dark:text-zinc-100">
                 17/08/2024
                 <span className="sr-only">Data</span>
               </h2>
 
               <p className="mt-2 sm:mt-4">
-                <strong className="text-3xl font-bold text-zinc-500 dark:text-zinc-100 sm:text-4xl">
-                  Culto do Novo viver
+                <strong className="text-3xl font-bold text-zinc-500 dark:text-zinc-100">
+                  Próximo Culto
                 </strong>
               </p>
             </div>
@@ -47,16 +47,16 @@ export default function Cronograma() {
             </a>
           </div>
 
-          <div className="rounded-2xl border border-zinc-200 p-6 shadow-sm sm:px-8 lg:p-12">
+          <div className="rounded-xl border border-zinc-200 p-6 shadow-sm sm:px-8 lg:p-12">
             <div className="text-center">
-              <h2 className="text-lg font-medium text-zinc-500 dark:text-zinc-100">
-                10/09/2024
+              <h2 className="text-sm font-medium text-zinc-500 dark:text-zinc-100">
+                10/08/2024
                 <span className="sr-only">Data</span>
               </h2>
 
               <p className="mt-2 sm:mt-4">
-                <strong className="text-3xl font-bold text-zinc-500 dark:text-zinc-100 sm:text-4xl">
-                  Culto de Missões
+                <strong className="text-2xl font-bold text-zinc-500 dark:text-zinc-100">
+                  Culto Anterior
                 </strong>
               </p>
             </div>
@@ -64,29 +64,8 @@ export default function Cronograma() {
             <ul className="mt-6 space-y-2">
               <li className="flex items-center gap-1">
                 <span className="text-zinc-500 dark:text-zinc-100">
-                  {" "}
-                  Agnus Dei - Orchestral
-                </span>
-              </li>
-
-              <li className="flex items-center gap-1">
-                <span className="text-zinc-500 dark:text-zinc-100">
-                  {" "}
-                  Louvores Irmão Lázaro
-                </span>
-              </li>
-
-              <li className="flex items-center gap-1">
-                <span className="text-zinc-500 dark:text-zinc-100">
-                  {" "}
-                  Medley!
-                </span>
-              </li>
-
-              <li className="flex items-center gap-1">
-                <span className="text-zinc-500 dark:text-zinc-100">
-                  {" "}
-                  Cem Ovelhas
+                  Live do culto realizada pelo Youtube, e disponível através do
+                  canal oficial da Igreja.
                 </span>
               </li>
             </ul>
--- app/pages/escola-de-musica/page.tsx
+++ app/pages/escola-de-musica/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
-import { Instagram } from "lucide-react";
+import {
+  GraduationCap,
+  Music,
+  Sprout,
+  Medal,
+  CalendarClock,
+  FishSymbol,
+  CirclePlus,
+  CircleMinus,
+} from "lucide-react";
 
 export default function EscolaDeMusica() {
   return (
@@ -7,22 +16,22 @@ export default function EscolaDeMusica() {
       <section>
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-            <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+            <div className="relative overflow-hidden rounded-xl sm:h-80 lg:order-last lg:h-full">
               <Image
                 src="/orchestra-3.png"
                 alt="Logo da Orquestra Shalom"
-                width={850}
-                height={850}
-                className="flex dark:invert"
+                width={1000}
+                height={600}
+                className="object-fill h-215"
               />
             </div>
 
             <div className="lg:py-24">
-              <h2 className="text-3xl font-bold sm:text-4xl">
+              <h2 className="text-3xl dark:text-zinc-100 font-extrabold sm:text-5xl">
                 Venha fazer parte da nossa família
               </h2>
 
-              <p className="mt-4 text-zinc-500 dark:text-zinc-100">
+              <p className="mt-4 text-zinc-500 dark:text-zinc-300">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut
                 qui hic atque tenetur quis eius quos ea neque sunt, accusantium
                 soluta minus veniam tempora deserunt? Molestiae eius quidem quam
@@ -43,11 +52,11 @@ export default function EscolaDeMusica() {
         <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
           <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
             <div className="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
-              <h2 className="text-3xl font-bold sm:text-4xl">
+              <h2 className="text-3xl dark:text-zinc-100 font-extrabold sm:text-5xl">
                 Vantagens de fazer parte da nossa escola
               </h2>
 
-              <p className="mt-4 text-zinc-500 dark:text-zinc-100">
+              <p className="mt-4 text-zinc-500 dark:text-zinc-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
                 vero aliquid sint distinctio iure ipsum cupiditate? Quis, odit
                 assumenda? Deleniti quasi inventore, libero reiciendis minima
@@ -60,29 +69,8 @@ export default function EscolaDeMusica() {
             </div>
 
             <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
-              <a
-                className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                href="#"
-              >
-                <span className="inline-block rounded-lg bg-gray-50 p-3">
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                    <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                    ></path>
-                  </svg>
-                </span>
-
+              <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                <GraduationCap className="text-zinc-500 dark:text-zinc-100"/>
                 <h2 className="mt-2 font-bold">Accountant</h2>
 
                 <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-zinc-500 dark:text-zinc-100">
@@ -90,29 +78,8 @@ export default function EscolaDeMusica() {
                 </p>
               </a>
 
-              <a
-                className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                href="#"
-              >
-                <span className="inline-block rounded-lg bg-gray-50 p-3">
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                    <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                    ></path>
-                  </svg>
-                </span>
-
+              <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                <Music className="text-zinc-500 dark:text-zinc-100"/>
                 <h2 className="mt-2 font-bold">Accountant</h2>
 
                 <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-zinc-500 dark:text-zinc-100">
@@ -120,29 +87,8 @@ export default function EscolaDeMusica() {
                 </p>
               </a>
 
-              <a
-                className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                href="#"
-              >
-                <span className="inline-block rounded-lg bg-gray-50 p-3">
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                    <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                    ></path>
-                  </svg>
-                </span>
-
+              <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                <Sprout className="text-zinc-500 dark:text-zinc-100"/>
                 <h2 className="mt-2 font-bold">Accountant</h2>
 
                 <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-zinc-500 dark:text-zinc-100">
@@ -150,29 +96,8 @@ export default function EscolaDeMusica() {
                 </p>
               </a>
 
-              <a
-                className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                href="#"
-              >
-                <span className="inline-block rounded-lg bg-gray-50 p-3">
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                    <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                    ></path>
-                  </svg>
-                </span>
-
+              <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                <Medal className="text-zinc-500 dark:text-zinc-100"/>
                 <h2 className="mt-2 font-bold">Accountant</h2>
 
                 <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-zinc-500 dark:text-zinc-100">
@@ -180,29 +105,8 @@ export default function EscolaDeMusica() {
                 </p>
               </a>
 
-              <a
-                className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                href="#"
-              >
-                <span className="inline-block rounded-lg bg-gray-50 p-3">
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                    <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                    ></path>
-                  </svg>
-                </span>
-
+              <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                <CalendarClock className="text-zinc-500 dark:text-zinc-100"/>
                 <h2 className="mt-2 font-bold">Accountant</h2>
 
                 <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-zinc-500 dark:text-zinc-100">
@@ -210,29 +114,8 @@ export default function EscolaDeMusica() {
                 </p>
               </a>
 
-              <a
-                className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                href="#"
-              >
-                <span className="inline-block rounded-lg bg-gray-50 p-3">
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                    <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                    ></path>
-                  </svg>
-                </span>
-
+              <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+                <FishSymbol className="text-zinc-500 dark:text-zinc-100"/>
                 <h2 className="mt-2 font-bold">Accountant</h2>
 
                 <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-zinc-500 dark:text-zinc-100">
@@ -243,50 +126,23 @@ export default function EscolaDeMusica() {
           </div>
         </div>
       </section>
-      <div className="space-y-4">
+      <div className="space-y-4 pb-6">
         <details
-          className="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+          className="group rounded-lg bg-zinc-100 dark:bg-zinc-900 p-6 [&_summary::-webkit-details-marker]:hidden"
           open
         >
           <summary className="flex cursor-pointer items-center justify-between gap-1.5 text-zinc-500 dark:text-zinc-100">
-            <h2 className="font-medium">
+            <h2 className="font-semibold">
               Lorem ipsum dolor sit amet consectetur adipisicing?
             </h2>
 
             <span className="relative size-5 shrink-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute inset-0 size-5 opacity-100 group-open:opacity-0"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute inset-0 size-5 opacity-0 group-open:opacity-100"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+              <CirclePlus className="absolute inset-0 opacity-100 group-open:opacity-0" />
+              <CircleMinus className="absolute inset-0 opacity-0 group-open:opacity-100" />
             </span>
           </summary>
 
-          <p className="mt-4 leading-relaxed text-zinc-500 dark:text-zinc-100">
+          <p className="mt-4 font-light leading-relaxed text-zinc-500 dark:text-zinc-300">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
             veritatis molestias culpa in, recusandae laboriosam neque aliquid
             libero nesciunt voluptate dicta quo officiis explicabo consequuntur
@@ -294,46 +150,19 @@ export default function EscolaDeMusica() {
           </p>
         </details>
 
-        <details className="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+        <details className="group rounded-lg bg-zinc-100 dark:bg-zinc-900 p-6 [&_summary::-webkit-details-marker]:hidden">
           <summary className="flex cursor-pointer items-center justify-between gap-1.5 text-zinc-500 dark:text-zinc-100">
-            <h2 className="font-medium">
+            <h2 className="font-semibold">
               Lorem ipsum dolor sit amet consectetur adipisicing?
             </h2>
 
             <span className="relative size-5 shrink-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute inset-0 opacity-100 group-open:opacity-0"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute inset-0 opacity-0 group-open:opacity-100"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+              <CirclePlus className="absolute inset-0 opacity-100 group-open:opacity-0" />
+              <CircleMinus className="absolute inset-0 opacity-0 group-open:opacity-100" />
             </span>
           </summary>
 
-          <p className="mt-4 leading-relaxed text-zinc-500 dark:text-zinc-100">
+          <p className="mt-4 font-light leading-relaxed text-zinc-500 dark:text-zinc-300">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
             veritatis molestias culpa in, recusandae laboriosam neque aliquid
             libero nesciunt voluptate dicta quo officiis explicabo consequuntur
